b66da79ac9eb8efd3f49e45976d70ab1ea8057e6
SrivatsaMG <srivatsamg@gmail.com>
2025-11-02 19:42:52 +0530

    Update default.js

--- components/context menus/default.js
+++ components/context menus/default.js
@@ -7,7 +7,7 @@ function DefaultMenu(props) {
                 <span className="ml-5">🌐</span> <span className="ml-2">Visit Portfolio</span>
             </a>
             <Devider />
-            <a rel="noreferrer noopener" href="https://www.linkedin.com/in/srivatsa-m-g" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
+            <a rel="noreferrer noopener" href="https://www.linkedin.com/in/srivatsamg/" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">🙋‍♂️</span> <span className="ml-2">Follow on <strong>Linkedin</strong></span>
             </a>
             <a rel="noreferrer noopener" href="https://github.com/srivatsamg" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
